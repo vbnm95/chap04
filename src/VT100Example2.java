6d430915fc02782e8d4f8d9a1a236bf27aafb384
vbnm95 <vbnm95@naver.com>
2023-07-13 18:03:31 +0900

    VT100 글자색 배경색 수정

--- src/VT100Example2.java
+++ src/VT100Example2.java
@@ -1,26 +1,29 @@
 
 public class VT100Example2 {
 	public static void main(String[] args) throws InterruptedException {
+		System.out.printf("\033[0m");
+		System.out.printf("\033[2J");
+		
 		for(int i=0; i<5000; i++) {
-			System.out.print("\033[2J");
-			System.out.print("\033[0m");
-			
 			int line = (int)(Math.random()*20 + 1);
 			int column = (int)(Math.random()*40 + 1);
-			int fg = (int)(Math.random()*8 + 30);
-			int bg = (int)(Math.random()*8 + 40);
+			//int fg = (int)(Math.random()*8 + 30);
+			//int bg = (int)(Math.random()*8 + 40);
+			int fg = 0;
+			int bg = 0;
 			char ch = (char)(Math.random()*26 + 'A');
 			
-			//System.out.printf("\033[%d;%dH", line, column);
-			//System.out.printf("\033[%dm", fg);
-			//System.out.printf("\033[%dm", bg);
-			
-			System.out.printf("\033[%d;%dH", line, column);
-	       System.out.printf("\033[%dm", fg);
-	       System.out.printf("\033[%dm", bg);
-	       System.out.print(ch);
-	       Thread.sleep(10);
-	       
+			while(true) {
+				int randomFg = (int)(Math.random()*8 + 30);
+				int randomBg = (int)(Math.random()*8 + 40);
+				if(randomFg%10 == randomBg%10) {
+					continue;
+				} else {
+					fg = randomFg;
+					bg = randomBg;
+					break;
+				}
+			}
 			/*
 	        System.out.print("\033[2J");        // 1. Clear Screen
 	        System.out.print("\033[10;10H");    // 2. Cursor Move
@@ -29,6 +32,17 @@ public class VT100Example2 {
 	        System.out.print("Hello VT100!");
 	        System.out.print("\033[0m");        // 5. Reset Color
 			*/
+			
+			System.out.printf("\033[%d;%dH", line, column);
+			System.out.printf("\033[%dm", fg);
+			System.out.printf("\033[%dm", bg);
+			System.out.printf("%c", ch);
+			Thread.sleep(10);
+	       
+			System.out.printf("\033[1;41H");
+			System.out.printf("\033[0m");
+			System.out.printf("i = [%5d]", i);
+			Thread.sleep(10);
 		}
 		
 		System.out.print("\033[0m");
