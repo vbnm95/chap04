0bd5f0f3455152517d24c2845b88535553baac5d
vbnm95 <vbnm95@naver.com>
2023-07-14 18:13:57 +0900

    VT100 원모양으로 수정

--- src/VT100Example2.java
+++ src/VT100Example2.java
@@ -1,9 +1,18 @@
+/*
+System.out.print("\033[2J");        // 1. Clear Screen
+System.out.print("\033[10;10H");    // 2. Cursor Move
+System.out.print("\033[31m");       // 3. Foreground Color
+System.out.print("\033[44m");       // 4. Background Color
+System.out.print("Hello VT100!");
+System.out.print("\033[0m");        // 5. Reset Color
+*/
 
 public class VT100Example2 {
 	public static void main(String[] args) throws InterruptedException {
 		System.out.printf("\033[0m");
 		System.out.printf("\033[2J");
 		
+		/*
 		for(int i=0; i<5000; i++) {
 			int line = (int)(Math.random()*20 + 1);
 			int column = (int)(Math.random()*40 + 1);
@@ -24,14 +33,6 @@ public class VT100Example2 {
 					break;
 				}
 			}
-			/*
-	        System.out.print("\033[2J");        // 1. Clear Screen
-	        System.out.print("\033[10;10H");    // 2. Cursor Move
-	        System.out.print("\033[31m");       // 3. Foreground Color
-	        System.out.print("\033[44m");       // 4. Background Color
-	        System.out.print("Hello VT100!");
-	        System.out.print("\033[0m");        // 5. Reset Color
-			*/
 			
 			System.out.printf("\033[%d;%dH", line, column);
 			System.out.printf("\033[%dm", fg);
@@ -44,9 +45,43 @@ public class VT100Example2 {
 			System.out.printf("i = [%5d]", i);
 			Thread.sleep(10);
 		}
+		*/
+		
+		for(int i=0; i<5000; i++) {
+			int line = (int)(Math.random()*31 - 15);
+			int column = (int)(Math.random()*31 - 15);
+			int fg = 0;
+			int bg = 0;
+			char ch = (char)(Math.random()*26 + 'A');
+			
+			while(true) {
+				int randomFg = (int)(Math.random()*8 + 30);
+				int randomBg = (int)(Math.random()*8 + 40);
+				if(randomFg%10 == randomBg%10) {
+					continue;
+				} else {
+					fg = randomFg;
+					bg = randomBg;
+					break;
+				}
+			}
+			
+			if(line*line + column*column < 15*15 ) {
+				System.out.printf("\033[%d;%dH", line+15, column+15);
+				System.out.printf("\033[%dm", fg);
+				System.out.printf("\033[%dm", bg);
+				System.out.printf("%c", ch);
+				Thread.sleep(10);
+				
+				System.out.printf("\033[1;31H");
+				System.out.printf("\033[0m");
+				System.out.printf("i = [%5d]", i);
+				Thread.sleep(10);
+			}
+		}
 		
 		System.out.print("\033[0m");
-		System.out.print("\033[21;1H");
+		System.out.print("\033[31;1H");
 		System.out.print("Program End");
 	}
 }
